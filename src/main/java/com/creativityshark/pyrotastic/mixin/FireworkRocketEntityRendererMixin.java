51de763b997f02000afa8a973971eaa4107cfb82
CreativityShark <ultrasharkcraft+github@gmail.com>
2022-02-26 13:06:07 -0600

    Made firework rocket renderer mixin less destructive

--- src/main/java/com/creativityshark/pyrotastic/mixin/FireworkRocketEntityRendererMixin.java
+++ src/main/java/com/creativityshark/pyrotastic/mixin/FireworkRocketEntityRendererMixin.java
@@ -13,11 +13,13 @@ import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.projectile.FireworkRocketEntity;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Quaternion;
 import net.minecraft.util.math.Vec3f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 //I just sort of messed around with the trident renderer code until it sort of worked?? IDK??
 @Mixin(FireworkRocketEntityRenderer.class)
@@ -32,10 +34,10 @@ public abstract class FireworkRocketEntityRendererMixin extends EntityRenderer<F
     @Inject(method = "<init>(Lnet/minecraft/client/render/entity/EntityRendererFactory$Context;)V", at = @At("TAIL"))
     private void constructorInject(EntityRendererFactory.Context context, CallbackInfo info) {
         this.model = new FireworkRocketModel(context.getPart(PyrotasticClient.FIREWORK_ROCKET_MODEL_LAYER));
-        
     }
 
-    public void render(FireworkRocketEntity fireworkRocketEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i) {
+    @Inject(method = "render(Lnet/minecraft/entity/projectile/FireworkRocketEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At("HEAD"), cancellable = true)
+    private void renderInject(FireworkRocketEntity fireworkRocketEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
         matrixStack.push();
         if(fireworkRocketEntity.wasShotAtAngle()) {
             matrixStack.multiply(this.dispatcher.getRotation());
@@ -47,9 +49,11 @@ public abstract class FireworkRocketEntityRendererMixin extends EntityRenderer<F
         this.model.render(matrixStack, vertexConsumer, i, OverlayTexture.DEFAULT_UV, 1.0F, 1.0F, 1.0F, 1.0F);
         matrixStack.pop();
         super.render(fireworkRocketEntity, f, g, matrixStack, vertexConsumerProvider, i);
+        ci.cancel();
     }
 
-    public Identifier getTexture(FireworkRocketEntity fireworkRocketEntity) {
-        return TEXTURE;
+    @Inject(method = "getTexture(Lnet/minecraft/entity/projectile/FireworkRocketEntity;)Lnet/minecraft/util/Identifier;", at = @At("RETURN"), cancellable = true)
+    private void getTextureInject(FireworkRocketEntity fireworkRocketEntity, CallbackInfoReturnable<Identifier> cir) {
+        cir.setReturnValue(TEXTURE);
     }
 }
